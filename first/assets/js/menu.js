d63f60f6c71193531193d4386f7bfb92693ce94a
jedart-official <mhaker2004@gmail.com>
2021-09-24 16:23:24 +0600

    first

---
+++ first/assets/js/menu.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".burger-menu").click(function(event) {
+        $(".burger-header").toggleClass("hide")
+        $(".burger-menu").toggleClass("active")
+
+        
+        
+
+
+    })
+});
