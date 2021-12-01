88b49aa4e7810bfaee4a2de0eb27c1b32bd8c8c2
jedart-official <mhaker2004@gmail.com>
2021-12-01 21:01:16 +0600

    1.1 Volleyboll

--- js/index.js
+++ js/index.js
@@ -1,6 +1,40 @@
-$('.multiple-items').slick({
-    infinite: true,
-    dots: true,
-    slidesToShow: 3,
-    slidesToScroll: 1
-  });
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 3,
+
+  
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+
+
+});
+
+const swiper1 = new Swiper('.swiper1', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 3,
+  freeMode: true,
+  
+  navigation: {
+    nextEl: '.swiper-button-next1',
+    prevEl: '.swiper-button-prev1',
+  },
+
+
+
+
+});
+
