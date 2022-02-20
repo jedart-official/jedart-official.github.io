8ca876b756cd72b808d48d22bc4d5b150d5234e0
jedart-official <mhaker2004@gmail.com>
2022-02-20 22:49:58 +0600

    library 1

---
+++ library/assets/js/swiper.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 100,
+    loop: true,
+    speed: 700,
+ 
+    
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+
+ 
+  });
