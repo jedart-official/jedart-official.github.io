ddcead41566ff5f6aaf6d6b36dd5dbf2a5b07cd3
jedart-official <mhaker2004@gmail.com>
2022-02-27 14:42:48 +0600

    clear

--- assets/js/burger.js
+++
@@ -1,6 +0,0 @@
-let burgerButton = document.querySelector(".header_burger")
-let burgerMenu = document.querySelector(".mobile_menu")
-burgerButton.addEventListener("click", ()=>{
-    burgerMenu.classList.toggle("show")
-    burgerButton.classList.toggle("active")
-})
